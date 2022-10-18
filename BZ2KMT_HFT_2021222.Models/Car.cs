c9dd707563718e3b79892c5b4c986d9a9ce1bb8f
patkos.marton <patkosmarci@gmail.com>
2022-10-18 22:23:21 +0200

    FuelType changed to string

--- BZ2KMT_HFT_2021222.Models/Car.cs
+++ BZ2KMT_HFT_2021222.Models/Car.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BZ2KMT_HFT_2021222.Models
 {
-    public enum Fuel { Gasoline = 1, Diesel = 2, Ethanol = 3, Electricity  = 4}
     public class Car
     {
         [Key]
@@ -15,7 +14,7 @@ namespace BZ2KMT_HFT_2021222.Models
         public string Model { get; set; }
         [StringLength(50)]
         public string Type { get; set; }
-        public Fuel FuelType { get; set; }
+        public string FuelType { get; set; }
         [Required]
         [Range(1900, 2100)]
         public int ReleaseYear { get; set; }
@@ -34,7 +33,7 @@ namespace BZ2KMT_HFT_2021222.Models
             CarId = int.Parse(split[0]);
             Model = split[1];
             Type = split[2];
-            FuelType = (Fuel)int.Parse(split[3]);
+            FuelType = split[3];
             ReleaseYear = int.Parse(split[4]);
             BrandId = int.Parse(split[5]);
         }
