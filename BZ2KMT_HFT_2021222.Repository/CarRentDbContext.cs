2f4a684220e0952f33586aabfc2034182c6a5918
patkos.marton <patkosmarci@gmail.com>
2022-10-18 22:38:59 +0200

    database and seed set up in CarRentDbContext

--- BZ2KMT_HFT_2021222.Repository/CarRentDbContext.cs
+++ BZ2KMT_HFT_2021222.Repository/CarRentDbContext.cs
@@ -26,5 +26,33 @@ namespace BZ2KMT_HFT_2021222.Repository
                     .UseSqlServer(conn);
             }
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>(car => car
+                .HasOne<Brand>()
+                .WithMany()
+                .HasForeignKey(car => car.BrandId)
+                .OnDelete(DeleteBehavior.Cascade));
+
+            modelBuilder.Entity<Car>().HasData(new Car[]
+            {
+                new Car("1#Swift#Sport#Petrol#1989#2"),
+                new Car("2#Bora#Sedanl#Petro#2003#1"),
+                new Car("3#Astra#Combi#Petrol#1997#3")
+            });
+
+            modelBuilder.Entity<Brand>().HasData(new Brand[]
+            {
+                new Brand("1#Volkswagen"),
+                new Brand("2#Suzuki"),
+                new Brand("3#Opel")
+            });
+
+            modelBuilder.Entity<Loans>().HasData(new Loans[]
+            {
+
+            });
+        }
     }
 }
