649a4923424ab5c340c400e53cd177dbec0e450e
patkos.marton <patkosmarci@gmail.com>
2022-10-26 09:30:04 +0200

    menu in client created and tested

--- BZ2KMT_HFT_2021222.Client/Program.cs
+++ BZ2KMT_HFT_2021222.Client/Program.cs
@@ -1,4 +1,7 @@
-﻿using BZ2KMT_HFT_2021222.Repository;
+﻿using BZ2KMT_HFT_2021222.Logic;
+using BZ2KMT_HFT_2021222.Logic.Classes;
+using BZ2KMT_HFT_2021222.Repository;
+using ConsoleTools;
 using System;
 using System.IO;
 using System.Linq;
@@ -7,11 +10,80 @@ namespace BZ2KMT_HFT_2021222.Client
 {
     internal class Program
     {
+        static CarLogic carLogic;
+        static BrandLogic brandLogic;
+        static LoanLogic loanLogic;
+        static void List(string entity)
+        {
+            if(entity == "Car")
+            {
+                var items = carLogic.ReadAll();
+                Console.WriteLine("Id\tBrandName\tModel");
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item.CarId + "\t" + item.Model);
+                }
+            }
+            Console.ReadLine();
+        }
+        static void Update(string entity)
+        {
+            Console.WriteLine(entity + " update");
+            Console.ReadLine();
+        }
+        static void Create(string entity)
+        {
+            Console.WriteLine(entity + " create");
+            Console.ReadLine();
+        }
+        static void Delete(string entity)
+        {
+            Console.WriteLine(entity + " delete");
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
-            //CarRentDbContext ctx = new CarRentDbContext();
+            var ctx = new CarRentDbContext();
 
-            //ctx.Brand.ToList().ForEach(t => Console.WriteLine(t.BrandName));
+            var carRepository = new CarRepository(ctx);
+            var brandRepository = new BrandRepository(ctx);
+            var loanRepository = new LoanRepository(ctx);
+
+            carLogic = new CarLogic(carRepository);
+            brandLogic = new BrandLogic(brandRepository);
+            loanLogic = new LoanLogic(loanRepository);
+
+            var carSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Car"))
+                .Add("Create", () => Create("Car"))
+                .Add("Delete", () => Delete("Car"))
+                .Add("Update", () => Update("Car"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var brandSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Brand"))
+                .Add("Create", () => Create("Brand"))
+                .Add("Delete", () => Delete("Brand"))
+                .Add("Update", () => Update("Brand"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var loanSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Loan"))
+                .Add("Create", () => Create("Loan"))
+                .Add("Delete", () => Delete("Loan"))
+                .Add("Update", () => Update("Loan"))
+                .Add("Exit", ConsoleMenu.Close);
+
+
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Cars", () => carSubMenu.Show())
+                .Add("Brand", () => brandSubMenu.Show())
+                .Add("Loan", () => loanSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            menu.Show();
+                
         }
     }
 }
