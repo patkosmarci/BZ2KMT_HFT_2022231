1d6b4ebb7a412b8de3119716dac15b5fd2cf7085
patkos.marton <patkosmarci@gmail.com>
2022-11-27 19:38:09 +0100

    API Controllers created

---
+++ BZ2KMT_HFT_2021222.Endpoint/Controllers/BrandController.cs
@@ -0,0 +1,56 @@
+﻿using BZ2KMT_HFT_2021222.Logic.Interfaces;
+using BZ2KMT_HFT_2021222.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BZ2KMT_HFT_2021222.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        IBrandLogic logic;
+
+        public BrandController(IBrandLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET: api/<BrandController>
+        [HttpGet]
+        public IEnumerable<Brand> ReadAll()
+        {
+            return logic.ReadAll();
+        }
+
+        // GET api/<BrandController>/5
+        [HttpGet("{id}")]
+        public Brand Read(int id)
+        {
+            return logic.Read(id);
+        }
+
+        // POST api/<BrandController>
+        [HttpPost]
+        public void Brand([FromBody] Brand value)
+        {
+            logic.Create(value);
+        }
+
+        // PUT api/<BrandController>/5
+        [HttpPut]
+        public void Update([FromBody] Brand value)
+        {
+            logic.Update(value);
+        }
+
+        // DELETE api/<BrandController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            logic.Delete(id);
+        }
+    }
+}
---
+++ BZ2KMT_HFT_2021222.Endpoint/Controllers/LoanController.cs
@@ -0,0 +1,56 @@
+﻿using BZ2KMT_HFT_2021222.Logic.Interfaces;
+using BZ2KMT_HFT_2021222.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BZ2KMT_HFT_2021222.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoanController : ControllerBase
+    {
+        ILoanLogic logic;
+
+        public LoanController(ILoanLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET: api/<LoanController>
+        [HttpGet]
+        public IEnumerable<Loan> ReadAll()
+        {
+            return logic.ReadAll();
+        }
+
+        // GET api/<LoanController>/5
+        [HttpGet("{id}")]
+        public Loan Read(int id)
+        {
+            return logic.Read(id);
+        }
+
+        // POST api/<LoanController>
+        [HttpPost]
+        public void Create([FromBody] Loan value)
+        {
+            logic.Create(value);
+        }
+
+        // PUT api/<LoanController>/5
+        [HttpPut]
+        public void Update([FromBody] Loan value)
+        {
+            logic.Update(value);
+        }
+
+        // DELETE api/<LoanController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            logic.Delete(id);
+        }
+    }
+}
